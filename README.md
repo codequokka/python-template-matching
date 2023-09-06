613b5fcab46f1a1ce0204e07753623a156e23023
Nobuchika Tanaka <codequokka@gmail.com>
2023-09-07 05:47:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vscode-devcontainer-python# python-template-matching
---
+++ src/template_matching.py
@@ -0,0 +1,32 @@
+import sys
+import cv2
+
+def pattern_matching(image_path: str, template_path: str) -> None:
+    image = cv2.imread(image_path)
+    template = cv2.imread(template_path)
+
+    w, h, _ = template.shape
+
+    result = cv2.matchTemplate(image, template, cv2.TM_CCORR_NORMED)
+
+    # 検索結果の信頼度と位置座標の取得
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    print(min_val, max_val, min_loc, max_loc)
+
+    # 検索結果の左上頂点の座標を取得
+    top_left = max_loc
+
+    # 検索結果の右下頂点の座標を取""
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    # 検索対象画像内に、検索結果を長方形で描画
+    cv2.rectangle(image, top_left, bottom_right, (0, 0, 255), 2)
+
+    cv2.imwrite("results/result.jpg", image)
+
+
+if __name__ == "__main__":
+    image_path = sys.argv[1]
+    template_path = sys.argv[2]
+    pattern_matching(image_path, template_path)
