116e26b02cb65c0c4741d473bd23189070013410
Nobuchika Tanaka <codequokka@gmail.com>
2023-09-08 06:15:06 +0900

    Temp commit

--- src/lib/pattern_match2.py
+++ src/lib/pattern_match2.py
@@ -4,8 +4,10 @@ import cv2
 def pattern_match(image_path: str, template_path: str) -> None:
     image = cv2.imread(image_path)
     template = cv2.imread(template_path)
+    image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    template_gs = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
-    result = cv2.matchTemplate(image, template, cv2.TM_CCORR_NORMED)
+    result = cv2.matchTemplate(image_gs, template_gs, cv2.TM_CCORR_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     print(min_val, max_val, min_loc, max_loc)
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import sys
 
-from lib.pattern_match import pattern_match
+from lib.pattern_match2 import pattern_match
+
+# from lib.pattern_match import pattern_match
 
 if __name__ == "__main__":
     image_path = sys.argv[1]
